da4eabe287804ff645862c202ba7ff8e1f1f311a
laurenpengyan <enzopeng1226@gmail.com>
2019-02-16 17:16:50 -0800

    Commit the local changes to javafx

--- javafx/src/Quotes.java
+++ javafx/src/Quotes.java
@@ -8,7 +8,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.util.Random;
 
 public class Quotes extends Application {
