317dec5d542243ad500d68d5f3586215f70a79f1
laurenpengyan <enzopeng1226@gmail.com>
2019-02-27 14:29:09 -0800

    Parsed JSON data. Then created HashMaps and answered the two questions regarding the state awards and recipients.

---
+++ BigData/src/School.java
@@ -0,0 +1,65 @@
+public class School implements Comparable<School> {
+
+    private long id;
+    private String name;
+    private String state;
+    private long awards;
+    private long recipients;
+
+    public School(long id, String name, String state, long awards, long recipients) {
+        this.id = id;
+        this.awards = awards;
+        this.recipients = recipients;
+        this.name = name;
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == this)
+            return true;
+
+        if (!(obj instanceof School)){
+            return false;
+        }
+
+        return this.id == ((School) obj).id;
+
+    }
+
+    @Override
+    public int compareTo(School school) {
+
+        if (school == this)
+            return 0;
+
+        return Long.compare(id, school.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "School: " + name + "\n State: " + state + "\n Awards: " + awards + "\n Recipients: " + recipients;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public long getAwards() {
+        return awards;
+    }
+
+    public long getRecipients() {
+        return recipients;
+    }
+
+}
